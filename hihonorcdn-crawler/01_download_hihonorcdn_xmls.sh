8d9605a8637f7cbe9bc8518ad63c9bba2fa71493
VuongDucAnh281107 <163089333+VuongDucAnh281107@users.noreply.github.com>
2024-08-12 08:02:35 +0700

    380000-420000

--- hihonorcdn-crawler/01_download_hihonorcdn_xmls.sh
+++ hihonorcdn-crawler/01_download_hihonorcdn_xmls.sh
@@ -3,8 +3,8 @@
 # Script tries to find all update files on update.hihonorcdn.com (filelist.xml & changelog.xml) and
 # stores those files into update_hihonorcdn_com_all_* directory for further processing
 
-from=100000
-to=400000
+from=380000
+to=420000
 
 d="update_hihonorcdn_com_all_$from-$to"
 
